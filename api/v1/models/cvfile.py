8b3f2e85beea7767bf7814fb2a2c637a1a4a95a9
Med. A. Bada <medbada.se@gmail.com>
2025-08-29 17:13:41 +0100

    ADD: CV File Model

---
+++ api/v1/models/cvfile.py
@@ -0,0 +1,18 @@
+"""
+CV File's Table Model
+
+Defines the `CV` File's table using SQLModel.
+"""
+
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class QRCode(SQLModel, table=True):
+    id: Optional[int] = Field(
+        default=None,
+        primary_key=True,
+        index=True,
+    )
+    url: str = Field(default=None, nullable=False)
